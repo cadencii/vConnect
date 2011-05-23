9837d8a98a3852d13f3e0be58238f1925ec41071
kbinani <kbinani@9d680d4f-cbfa-477e-9905-73dbafb9b024>
2011-05-23 17:44:20 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/cadencii/vConnect/trunk@1623 9d680d4f-cbfa-477e-9905-73dbafb9b024

--- vcnctd/vcnctd.h
+++ vcnctd/vcnctd.h
@@ -4,5 +4,54 @@
  * Copyright © 2011 kbinani.
  */
 
+#ifndef __vcnctd_h__
+
 #include "../stand/standData.h"
+#include "../stand/mb_text.h"
 #include "../stand/vConnect.h"
+#include "../stand/corpusManager.h"
+#include "../stand/standFrame.h"
+#include "../stand/vsqMetaText/vsqBase.h"
+#include "../stand/vsqMetaText/vsqTempo.h"
+#include "../stand/vsqMetaText/vsqPhonemeDB.h"
+#include "../stand/vsqMetaText/vsqHandle.h"
+#include "../stand/vsqMetaText/vsqFileEx.h"
+#include "../stand/utauVoiceDB/UtauDB.h"
+#include "../stand/vsqMetaText/vsqEventList.h"
+#include "../stand/vsqMetaText/vsqLyric.h"
+#include "../stand/vsqMetaText/vsqEventEx.h"
+#include "../stand/vowelTable.h"
+#include "../stand/vsqMetaText/vsqBPList.h"
+#include "../stand/standSpecgram.h"
+#include "../stand/world/world.h"
+#include "../stand/waveFileEx/waveFileEx.h"
+#include "../stand/matching.h"
+#include "../stand/vConnectSetting.h"
+#include "../stand/standMelCepstrum.h"
+#include "../stand/stand.h"
+
+namespace vcnctd
+{
+    
+    namespace vsq
+    {
+        typedef vConnect Synthesizer;
+        typedef runtimeOptions Options;
+        typedef vsqEventList EventList;
+        typedef vsqEventEx Event;
+        typedef vsqLyric Lyric;
+        typedef vsqHandle Handle;
+        typedef vsqFileEx Sequence;
+    }
+    
+    namespace stand
+    {
+        typedef standMelCepstrum MelCeps;
+        typedef standSpecgram Specgram;
+        typedef standFrame Frame;
+        typedef standData BankSource;
+    }
+
+}
+
+#endif
